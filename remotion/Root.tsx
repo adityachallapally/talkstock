e3f65089f7f6a403868f7473d2e78e411606b092
Aditya Challapally <avasanthc@gmail.com>
2025-03-06 08:47:33 -0800

    updated root

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -9,7 +9,43 @@ import {
 import { CaptionedVideo, calculateCaptionedVideoMetadata } from "./CaptionedVideo/index";
 import { parseMedia } from "@remotion/media-parser";
 
+export enum TemplateType {
+  BULLET_LIST = 'BULLET_LIST',
+  WORD_SWAP = 'WORD_SWAP',
+  NUMBER_HIGHLIGHT = 'NUMBER_HIGHLIGHT',
+  STOCK_VIDEO = 'STOCK_VIDEO'
+}
+
+
 export const RemotionRoot: React.FC = () => {
+
+  const mockOverlays = [
+    {
+      startFrame: Math.round((8540 / 1000) * 30),
+      duration: Math.round((2000 / 1000) * 30),
+      type: TemplateType.STOCK_VIDEO,
+      videoSrc: 'https://videos.pexels.com/video-files/5532765/5532765-sd_506_960_25fps.mp4',
+      title: '',
+      provider: 'Pexels'
+    },
+    {
+      startFrame: Math.round((11800 / 1000) * 30),
+      duration: Math.round((2000 / 1000) * 30),
+      type: TemplateType.STOCK_VIDEO,
+      videoSrc: 'https://videos.pexels.com/video-files/5532765/5532765-sd_506_960_25fps.mp4',
+      title: '',
+      provider: 'Pexels'
+    },
+    {
+      startFrame: Math.round((14560 / 1000) * 30),
+      duration: Math.round((2000 / 1000) * 30),
+      type: TemplateType.STOCK_VIDEO,
+      videoSrc: 'https://videos.pexels.com/video-files/5532765/5532765-sd_506_960_25fps.mp4',
+      title: '',
+      provider: 'Pexels'
+    }
+  ];
+
   return (
     <>
       <Composition
@@ -20,9 +56,9 @@ export const RemotionRoot: React.FC = () => {
         width={VIDEO_WIDTH}
         height={VIDEO_HEIGHT}
         defaultProps={{
-          src: "placeholder-src",
-          overlays: [],
-          transcriptionUrl: "",
+          src: 'https://hx7mp5wayo6ybdwl.public.blob.vercel-storage.com/IMG_6062-ustELCsT8kuxTiuEmUhR0NTEefvx6P.MP4',
+          overlays: mockOverlays,
+          transcriptionUrl: 'https://hx7mp5wayo6ybdwl.public.blob.vercel-storage.com/transcript-YxnHCXJcmH4JJqN5LH4M7r79CprrIa.json',
           showCaptions: true
         }}
         calculateMetadata={async ({ props }) => {
